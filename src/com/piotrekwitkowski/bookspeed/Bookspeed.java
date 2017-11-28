b51fc4585e1eb8b6c2367990c6918b82f02eed03
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2017-11-28 13:46:41 +0100

    Initial commit

---
+++ src/com/piotrekwitkowski/bookspeed/Bookspeed.java
@@ -0,0 +1,90 @@
+package com.piotrekwitkowski.bookspeed;
+
+import java.util.ArrayList;
+
+public class Bookspeed {
+
+    private static ArrayList<Book> books = new ArrayList<>();
+
+    public static void main(String[] args) throws BookNotFoundException, InterruptedException {
+        int booksNumber = 100000;
+        System.out.println("Adding " + booksNumber + " books...");
+
+        for (int i = 0; i<booksNumber; i++) {
+            books.add(new Book(new BookDetails("ISBN", "author", "booktitle")));
+        }
+        System.out.println(booksNumber + " books added!");
+
+        books.add(new Book(new BookDetails("ISBN2", "author2", "Special Title")));
+        System.out.println((booksNumber+1) + " special book added!");
+
+        Thread.sleep(1000);
+        System.out.println("3...");
+        Thread.sleep(1000);
+        System.out.println("2...");
+        Thread.sleep(1000);
+        System.out.println("1...");
+        Thread.sleep(1000);
+
+        System.out.println("Looking for special book in O(n)...");
+        Book specialBook = Book.getBookByTitle(books, "Special Title");
+
+        if (specialBook != null) {
+            System.out.println(specialBook.getBookDetails().getTitle());
+        }
+    }
+
+    private static class Book {
+        private BookDetails bookDetails;
+        private int borrowsIndex;
+        private User currentUser;
+
+        Book(BookDetails bookDetails) {
+            this.bookDetails = bookDetails;
+            this.borrowsIndex = 0;
+        }
+
+        static Book getBookByTitle(ArrayList<Book> booksList, String bookname) throws BookNotFoundException {
+            for (Book book : booksList) {
+                if (book.getBookDetails().getTitle().equals(bookname)) {
+                    return book;
+                }
+            }
+            throw new BookNotFoundException();
+        }
+
+        BookDetails getBookDetails() {
+            return bookDetails;
+        }
+
+    }
+
+    private static class BookDetails {
+        private String isbn;
+        private String author;
+        private String title;
+
+        BookDetails(String isbn, String author, String title) {
+            this.isbn = isbn;
+            this.author = author;
+            this.title = title;
+        }
+
+        String getTitle() {
+            return title;
+        }
+
+    }
+
+    private static class BookNotFoundException extends Exception {
+    }
+
+    private class User {
+        private String name;
+        private int borrowedBooksIndex = 0;
+
+        User(String name) {
+            this.name = name;
+        }
+    }
+}
